6c0aed82636f20c2b28f78559a10c20fa38d91c0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-03-26 21:13:59 +0400

    Make Target class abstract

--- Target.php
+++ Target.php
@@ -12,28 +12,9 @@ use yii\helpers\Console;
 use yii\helpers\VarDumper;
 
 /**
- * Target is the base class for all log target classes.
- *
- * A log target object will filter the messages logged by [[Logger]] according
- * to its [[levels]] and [[categories]] properties. It may also export the filtered
- * messages to specific destination defined by the target, such as emails, files.
- *
- * Level filter and category filter are combinatorial, i.e., only messages
- * satisfying both filter conditions will be handled. Additionally, you
- * may specify [[except]] to exclude messages of certain categories.
- *
- * @property bool $enabled Indicates whether this log target is enabled. Defaults to true. Note that the type
- * of this property differs in getter and setter. See [[getEnabled()]] and [[setEnabled()]] for details.
- * @property int $levels The message levels that this target is interested in. This is a bitmap of level
- * values. Defaults to 0, meaning  all available levels. Note that the type of this property differs in getter
- * and setter. See [[getLevels()]] and [[setLevels()]] for details.
- *
- * For more details and usage information on Target, see the [guide article on logging & targets](guide:runtime-logging).
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
+ * @inheritdoc
  */
-class Target extends \yii\log\Target
+abstract class Target extends \yii\log\Target
 {
     /**
      * @var
@@ -90,15 +71,6 @@ class Target extends \yii\log\Target
             }
         }
         return $data;
-    }
-
-    /**
-     * Exports log [[messages]] to a specific destination.
-     * Child classes must implement this method.
-     */
-    public function export()
-    {
-        parent::init();
     }
 
     public function getFormatMessage()

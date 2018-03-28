dc68b604af0968033bd4838f4d38adb54afb97a3
Zura Tevdoradze <zusate@gmail.com>
2018-03-28 13:09:57 +0400

    Add feature , send email to EmailTarget Fix #2

--- EmailTarget.php
+++ EmailTarget.php
@@ -9,8 +9,45 @@
 namespace apollo11\logger;
 
 
+use Yii;
+use yii\base\InvalidConfigException;
+
 class EmailTarget extends Target
 {
+    /**
+     * 'components' => [
+     *     'log' => [
+     *          'targets' => [
+     *              [
+     *                  'class' => apollo11\logger\EmailTarget::class,
+     *                  'levels' => ['error'],
+     *                  'excludeKeys' => [],
+     *                  'message' => [
+     *                      'to' => 'example@example.com',
+     *                      'from' => 'example@example.com',
+     *                      'subject' => 'Apollo11 Error Logger',
+     *                  ]
+     *              ],
+     *          ],
+     *     ],
+     * ],
+     *
+     */
+
+    /**
+     * @var array
+     */
+    public $message = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        if (empty($this->message['to'])) {
+            throw new InvalidConfigException('The "to" option must be set for EmailTarget::message.');
+        }
+    }
 
     /**
      * Exports log [[messages]] to a specific destination.
@@ -18,6 +55,14 @@ class EmailTarget extends Target
      */
     public function export()
     {
+        if (empty($this->message['subject'])) $this->message['subject'] = 'Error Log';
+        if (empty($this->message['from'])) $this->message['from'] = 'test@test.test';
 
+        Yii::$app->mailer->compose()
+            ->setFrom($this->message['from'])
+            ->setTextBody($this->getFormatMessage())
+            ->setTo($this->message['to'])
+            ->setSubject($this->message['subject'])
+            ->send();
     }
 }

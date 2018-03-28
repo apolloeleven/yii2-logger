c262ed3592dab4124193c8f1736e0aec285be00d
Zura Tevdoradze <zusate@gmail.com>
2018-03-28 11:31:49 +0400

    Create EmailTarget class

---
+++ EmailTarget.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zura
+ * Date: 3/28/18
+ * Time: 11:28 AM
+ */
+
+namespace apollo11\logger;
+
+
+class EmailTarget extends Target
+{
+
+    /**
+     * Exports log [[messages]] to a specific destination.
+     * Child classes must implement this method.
+     */
+    public function export()
+    {
+
+    }
+}
